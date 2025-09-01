9d8561a9b9b38607c5c6406d6980546179f00372
Dylan Rauhoff <DylanRauhoff@gmail.com>
2025-09-01 00:51:38 -0500

    Trigger redeploy after Vercel Pro upgrade

--- api/status.js
+++ api/status.js
@@ -2,6 +2,7 @@ export default async function handler(req, res) {
   res.status(200).json({
     status: 'OK',
     timestamp: new Date().toISOString(),
-    message: 'Menu Editor API is working'
+    message: 'Menu Editor API is working',
+    version: '2.0'
   });
 }
